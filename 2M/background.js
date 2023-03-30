5ff88404481f552d6bbaf5e5cbd130385d572bdf
brd87 <115787728+brd87@users.noreply.github.com>
2023-03-30 17:13:54 +0200

    Add files via upload

--- 2M/background.js
+++ 2M/background.js
@@ -1,15 +1,7 @@
 chrome.downloads.onDeterminingFilename.addListener(function(item, suggest) {
-	let url = new URL(item.url);
-	let hostname = url.hostname;
-    let filepath = item.filename;
-    let ext = filepath.split('.').pop();
-    let now = new Date();
-    let date = now.getFullYear().toString() +
-              now.getMonth().toString().padStart(2, '0') +
-              now.getDate().toString().padStart(2, '0') +
-              now.getHours().toString().padStart(2, '0') +
-              now.getMinutes().toString().padStart(2, '0') +
-              now.getSeconds().toString().padStart(2, '0');
-	let Nfilename = hostname + '-' + date + '.' + ext;
+    let url = new URL(item.url);
+    let hostname = url.hostname;
+    let original = item.filename.split('/').pop()
+    let Nfilename = hostname + '-' + original;
     suggest({filename: Nfilename});
 });
